163afa1bd15bf5690c8f28b5db3fefeb014b0ae5
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2021-05-22 22:09:52 -0300

    Code format and cleanup

--- src/components/Interpreter.js
+++ src/components/Interpreter.js
@@ -13,7 +13,7 @@ export default function Interpreter() {
     failNext: 0,
   };
 
-  const [rules, setRules] = useState([{...blankRule}]);
+  const [rules, setRules] = useState([{ ...blankRule }]);
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
   const [running, setRunning] = useState(false);
@@ -27,8 +27,8 @@ export default function Interpreter() {
   const advanceStepRef = useRef(false);
 
   const ruleManager = {
-    addBlankRule(){
-      setRules(oldRules => [...oldRules, {...blankRule}]);
+    addBlankRule() {
+      setRules(oldRules => [...oldRules, { ...blankRule }]);
     },
 
     updateRule(id, updatedRule) {
@@ -37,7 +37,7 @@ export default function Interpreter() {
       setRules(newRules);
     },
 
-    deleteRule(id){
+    deleteRule(id) {
       const newRules = [...rules];
       newRules.splice(id, 1);
       setRules(newRules);
@@ -106,92 +106,95 @@ export default function Interpreter() {
     setAutoAdvance(!autoAdvance);
   }
 
+  function handleNextStep() {
+    advanceStepRef.current = true;
+  }
+
   return (
     <div className="flex flex-col my-10 w-4/5 m-auto">
       <h2 className="font-bold text-lg">Algorithm Rules</h2>
       <InterpreterContext.Provider value={ruleManager}>
-          <table className="table-fixed mb-2">
-            <thead>
-                <tr>
-                  <th className="text-center w-10">ID</th>
-                  <th className="text-left w-32">String origin</th>
-                  <th className="text-left w-32">String target</th>
-                  <th className="text-left w-60">Next instruction (Success)</th>
-                  <th className="text-left w-60">Next instruction (Fail)</th>
-                  <th className="text-center w-10">Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                {rules.map((rule, index) => (
-                  <Instruction 
-                    rule={rule} 
-                    id={index} 
-                    disabled={running} 
-                    highlight={index === currentRuleId} 
-                    success={currentRuleSuccess}
-                    key={index}
-                  />
-                ))}
-                <tr>
-                  <td className={"text-center rounded" + (currentRuleId === rules.length ? " bg-blue-300" : "")}>{rules.length}</td>
-                  <td colSpan="5">Done!</td>
-                </tr>
-            </tbody>
-          </table>
+        <table className="table-fixed mb-2">
+          <thead>
+            <tr>
+              <th className="text-center w-10">ID</th>
+              <th className="text-left w-32">String origin</th>
+              <th className="text-left w-32">String target</th>
+              <th className="text-left w-60">Next instruction (Success)</th>
+              <th className="text-left w-60">Next instruction (Fail)</th>
+              <th className="text-center w-10">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rules.map((rule, index) => (
+              <Instruction
+                rule={rule}
+                id={index}
+                disabled={running}
+                highlight={index === currentRuleId}
+                success={currentRuleSuccess}
+                key={index}
+              />
+            ))}
+            <tr>
+              <td className={"text-center rounded" + (currentRuleId === rules.length ? " bg-blue-300" : "")}>{rules.length}</td>
+              <td colSpan="5">Done!</td>
+            </tr>
+          </tbody>
+        </table>
       </InterpreterContext.Provider>
-      <button 
+      <button
         onClick={ruleManager.addBlankRule}
         className="flex items-center bg-green-400 px-2 py-1 rounded focus:outline-none hover:bg-green-500 w-40"
       >
-        <MdAdd size={32} display="inline"/>Add Instruction
+        <MdAdd size={32} display="inline" />Add Instruction
       </button>
 
       <div className="flex items-center my-5 w-4/5">
-        <input 
-            type="text" 
-            value={input} 
-            className="input"
-            onChange={event => setInput(event.target.value)} 
-            placeholder="Type the algorithm input"
-            disabled={running}
+        <input
+          type="text"
+          value={input}
+          className="input"
+          onChange={event => setInput(event.target.value)}
+          placeholder="Type the algorithm input"
+          disabled={running}
         />
-        {running && !autoAdvance && 
+        {running && !autoAdvance &&
           <button
             className="shadow rounded border border-gray-200 focus:outline-none flex items-center px-2 ml-2 bg-gray-400 active:bg-gray-600 hover:bg-gray-500 text-white h-8"
-            onClick={() => {advanceStepRef.current = true;}}
+            onClick={handleNextStep}
           >
             Step {stepCounter}
-            <MdChevronRight size={30}/>
+            <MdChevronRight size={30} />
             Next
           </button>
         }
 
-        <button 
-          onClick={running ? handleStop : handleStart }
+        <button
+          onClick={running ? handleStop : handleStart}
           className="shadow rounded border border-gray-200 focus:outline-none w-20 flex items-center px-2 ml-2 bg-gray-400 text-white active:bg-gray-600 hover:bg-gray-500"
         >
-          {running ? 
+          {running ?
             <>
-              <MdStop size={30}/> Stop
-            </> : 
+              <MdStop size={30} /> Stop
+            </> :
             <>
               <MdPlayArrow size={30} /> Start
             </>
           }
         </button>
 
-        {!running && 
+        {!running &&
           <label className="ml-2 shadow rounded border border-gray-200 focus:outline-none flex items-center px-2 ml-2 bg-gray-400 active:bg-gray-600 hover:bg-gray-500 text-white h-8 cursor-pointer">
-            <input 
-              type="checkbox" 
+            <input
+              type="checkbox"
               className="mr-2 cursor-pointer"
               checked={!autoAdvance}
-              onChange={handleAutoAdvanceChange}/> 
+              onChange={handleAutoAdvanceChange} />
               Step-by-step
           </label>
         }
-        
-        
+
       </div>
       <p>
         Output: {errorMessage ? <span className="text-red-500">Error - {errorMessage}</span> : <span>{output}</span>}
--- src/markovAlgorithm.js
+++ src/markovAlgorithm.js
@@ -1,51 +1,55 @@
 
 export function getMarkovAlgorithmProcessor(input, rules, maxStep) {
-  function* algorithmProcessor(inputString, rules, maxStep=100000) {
+  function* algorithmProcessor(inputString, rules, maxStep = 100000) {
     let currentString = inputString;
     let ruleId = 0;
     let currentStep = 1;
-  
+
     while (ruleId < rules.length) {
       if (currentStep > maxStep) {
         throw Error("Max steps reached");
       }
-  
+
       let rule = rules[ruleId];
-  
+
       if (currentString.indexOf(rule.originString) !== -1) {
         const processedString = currentString.replace(rule.originString, rule.targetString);
-        
+
         yield {
-          string: processedString, 
-          step: currentStep, 
-          currentRule: ruleId, 
+          string: processedString,
+          step: currentStep,
+          currentRule: ruleId,
           success: true
         };
 
         currentString = processedString;
         ruleId = rule.successNext;
-      
+
       } else {
 
         yield {
-          string: currentString, 
-          step: currentStep, 
-          currentRule: ruleId, 
+          string: currentString,
+          step: currentStep,
+          currentRule: ruleId,
           success: false
         };
-        
+
         ruleId = rule.failNext;
       }
-  
+
       currentStep += 1;
     }
-  
+
     if (ruleId > rules.length) {
       throw Error(`Instruction ${ruleId} does no exist`);
     }
-  
+
     if (ruleId === rules.length) {
-      yield {string: currentString, step: currentStep, currentRule: ruleId}
+      yield {
+        string: currentString,
+        step: currentStep,
+        currentRule: ruleId
+      }
     }
   }
 
@@ -58,17 +62,17 @@ export function getMarkovAlgorithmProcessor(input, rules, maxStep) {
     function runner() {
       try {
         const advance = shouldAdvance();
-  
+
         switch (advance) {
           case 'end':
             stopRunner();
             onFinish();
             break;
-  
+
           case 'next':
             const output = processor.next();
-  
-            if (output.done){
+
+            if (output.done) {
               stopRunner();
               onFinish();
             } else {
@@ -76,7 +80,7 @@ export function getMarkovAlgorithmProcessor(input, rules, maxStep) {
             }
             break;
           case 'wait':
-            // fall through
+          // fall through
           default:
             break;
         }
